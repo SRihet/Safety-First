5249eaa5d8fe7a762f3f027527c982770d10f544
SRihet <stephane.rihet@gmail.com>
2020-04-21 17:33:19 +0200

    Initial Commit

---
+++ Safety First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Safety First
+//
+//  Created by Stéphane Rihet on 21/04/2020.
+//  Copyright © 2020 SapinRocher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
